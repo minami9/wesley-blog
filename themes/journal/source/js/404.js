d230f2912013090851b95bab715b27a42c43c7c2
weiming <weiming@autoio.cn>
2020-06-14 16:01:38 +0800

    修改404.js为oop

--- themes/journal/source/js/404.js
+++ themes/journal/source/js/404.js
@@ -1,104 +1,116 @@
+var Console404 = {
+    createNew: function() {
+        var console404 = {};
+        var blinkTiming = 0;
+        var paragraphsId = "console-text-";
+        var currentParagraphsIndex = 0;
+        var currentSentensIndex = 0;
+        var currentChIndex = 0;
+        var sentence = new Array(
+            "code 404,",
+            "Hi,",
+            "You are lost in my site,",
+            "Please return to home page, :)."
+        );
 
-
-var blinkTiming = 0;
-var paragraphsId = "console-text-";
-var currentParagraphsIndex = 0;
-
-function blinkCursor()
-{
-    var blinkNode = document.getElementById("blink-block");
-
-    if (blinkNode == null)
-        return;
-
-    if (blinkTiming++ > 5) 
-        blinkNode.innerHTML = "_";
-    else 
-        blinkNode.innerHTML = "";
+        blinkCursor = function() {
+            var blinkNode = document.getElementById("blink-block");
     
-    if(blinkTiming >= 10) 
-        blinkTiming = 0;
-}
+            if (blinkNode == null)
+                return;
+    
+            if (blinkTiming++ > 5) 
+                blinkNode.innerHTML = "_";
+            else 
+                blinkNode.innerHTML = "";
+            
+            if(blinkTiming >= 10) 
+                blinkTiming = 0;
+        }
 
+        deleteBlinkBlock = function() {
+            var parentNode = document.getElementById(paragraphsId + currentParagraphsIndex);
+            if (parentNode != null) {
+                var blinkNode = document.getElementById("blink-block");
+                if (blinkNode)
+                    parentNode.removeChild(blinkNode);
+            }
+        }
+        
+        appendBlinkBlock = function(parentNode) {
+            var blinkNode = document.createElement("span");
+            blinkNode.id = "blink-block";
+            parentNode.appendChild(blinkNode);
+        }
 
-function deleteBlinkBlock()
-{
-    var parentNode = document.getElementById(paragraphsId + currentParagraphsIndex);
-    if (parentNode != null) {
-        var blinkNode = document.getElementById("blink-block");
-        if (blinkNode)
-            parentNode.removeChild(blinkNode);
+        consoleCommunications = function() {
+            if (currentSentensIndex >= sentence.length) {
+                currentSentensIndex = sentence.length;
+                return;
+            }
+        
+            if (currentChIndex >= sentence[currentSentensIndex].length) {
+                currentSentensIndex++;
+                currentChIndex = 0;
+                newParagraphs();
+                return;
+            }
+            deleteBlinkBlock();
+            var paragraphs = document.getElementById(paragraphsId + currentParagraphsIndex);
+        
+            var ch = sentence[currentSentensIndex].substring(currentChIndex, currentChIndex + 1);
+            if (ch === ' ')
+                ch = '\xa0';
+        
+            paragraphs.innerText = paragraphs.innerText + ch;//.toUpperCase();
+            appendBlinkBlock(paragraphs);
+        
+            currentChIndex++;
+        }
+
+        runConsole = function() {
+            setInterval(consoleCommunications, 100);
+        }
+    
+        newParagraphs = function() {
+            deleteBlinkBlock();
+            var parentNode = document.getElementById(paragraphsId + currentParagraphsIndex);
+            if (parentNode != null) 
+                currentParagraphsIndex++;
+            
+            var consoleWindow = document.getElementById("console-window");
+            var paragraphs = document.createElement("p");
+            paragraphs.innerHTML = ">&nbsp";
+            paragraphs.setAttribute("class", "console-text-class");
+            paragraphs.id = paragraphsId + currentParagraphsIndex;
+            appendBlinkBlock(paragraphs);
+            consoleWindow.appendChild(paragraphs);
+        }
+
+        console404.show = function() {
+            setInterval(blinkCursor, 150);
+            setTimeout(runConsole, 2500);
+            newParagraphs();
+        }
+        return console404;
     }
 }
 
-function appendBlinkBlock(parentNode)
-{
-    var blinkNode = document.createElement("span");
-    blinkNode.id = "blink-block";
-    parentNode.appendChild(blinkNode);
-}
+window.onload = function() {
+    var console404 = Console404.createNew();
+    console404.show();
+};
 
-function newParagraphs()
-{
-    deleteBlinkBlock();
-    var parentNode = document.getElementById(paragraphsId + currentParagraphsIndex);
-    if (parentNode != null) 
-        currentParagraphsIndex++;
-    
-    var consoleWindow = document.getElementById("console-window");
-    var paragraphs = document.createElement("p");
-    paragraphs.innerHTML = ">&nbsp";
-    paragraphs.setAttribute("class", "console-text-class");
-    paragraphs.id = paragraphsId + currentParagraphsIndex;
-    appendBlinkBlock(paragraphs);
-    consoleWindow.appendChild(paragraphs);
-}
 
-var sentence = new Array(
-    "code: 404,",
-    "hi,",
-    "you are lost in my site.",
-    "please return to home page."
-);
 
-var currentSentensIndex = 0;
-var currentChIndex = 0;
 
-function consoleCommunications()
-{
-    if (currentSentensIndex >= sentence.length) {
-        currentSentensIndex = sentence.length;
-        return;
-    }
-    
 
-    if (currentChIndex >= sentence[currentSentensIndex].length) {
-        currentSentensIndex++;
-        currentChIndex = 0;
-        newParagraphs();
-        return;
-    }
-    deleteBlinkBlock();
-    var paragraphs = document.getElementById(paragraphsId + currentParagraphsIndex);
 
-    var ch = sentence[currentSentensIndex].substring(currentChIndex, currentChIndex + 1);
-    if (ch === ' ')
-        ch = '\xa0';
 
-    paragraphs.innerText = paragraphs.innerText + ch;//.toUpperCase();
-    appendBlinkBlock(paragraphs);
 
-    currentChIndex++;
-}
 
-function runConsole()
-{
-    setInterval(consoleCommunications, 100);
-}
 
-function consoleShow()
-{
-    setInterval(blinkCursor, 100);
-    setTimeout(runConsole, 3000);
-    newParagraphs();
-}
+
+
+
+
